9d962efc5648d7fad53b8835e92dbec255ce1867
KarolinaCzapla <karolina.czapla@onet.com.pl>
2021-10-29 14:03:46 +0200

    all the def(artist) transferred to the new file(package)

--- top _spotify_songs/songs.py
+++ top _spotify_songs/songs.py
@@ -1,5 +1,6 @@
 import csv
 from matplotlib import pyplot as plt
+from allPackage.artistPackage import artist
 
 
 class all_top10:
@@ -19,48 +20,11 @@ with open('top10s.csv', 'r') as file:
     for row in reader:
         data.append(all_top10(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
 
-
-def artist_number(data):
-    word = []
-    for x in data[1:]:
-        word.append(x.artist)
-    return word
-
-
-def count_artist():
-    counts = dict()
-    for word in artist_number(data):
-        if word in counts:
-            counts[word] += 1
-        else:
-            counts[word] = 1
-
-    return counts
-
-
-# def print_key_value(counts):
-#     for k, v in counts.items():
-#         print(k, v)
-#
-# print_key_value(count_artist())
-
-
-def sorted_artist(counts):
-    sort_orders = sorted(counts.items(), key=lambda x: x[1], reverse=True)
-    for i in sort_orders:
-        print(i[0], i[1])
-    return sort_orders
-
-
-sorted_artist(count_artist())
-
-
-def top_ten(sort_orders):
-    for i in sort_orders[:10]:
-        print(i[0], i[1])
-
-
-top_ten(sorted_artist(count_artist()))
+# artist.all_artist(data)
+artist.count_artist(data)
+# artist.print_key_value(data)
+artist.sorted_artist(data)
+artist.top_ten(data)
 
 # def type_music():
 #     type = []
